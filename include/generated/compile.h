39ae2f9fa7d85285e6e874d4be68c3002b0899bb
Chickenboy1703 <chipchipquadepchai@gmail.com>
2022-07-29 20:03:46 +0700

    Stock on samsung opensources

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#3 SMP PREEMPT Fri Jul 29 17:03:24 +07 2022"
+#define LINUX_COMPILE_BY "natsu"
+#define LINUX_COMPILE_HOST "natsuPC"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
